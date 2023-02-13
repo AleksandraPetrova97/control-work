364181e9f6150bd56da7d1d04297a76d1e65ba29
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-02-13 17:35:08 +0300

    Сохранаю

---
+++ my_programs/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу, которая из имеющигося массива строк
+//формирует массив из строк, длина которых меньше 
+//либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+
+string [] GetArray (int length, int start, int end)
+{
+    string [] array = new string [length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+
+void printArray(string[] array)
+{
+    Console.Write($"В массиве [");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length -1)
+        {
+        Console.Write($"{array[i]},");
+        }
+        else
+        {
+        Console.WriteLine($"{array[i]}] - {Sumnum} чётных чисел");
+        }
+    }
+}
